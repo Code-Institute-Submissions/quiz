389bf37762ca4cba9ca3c5ff5d10b0d4a4059ee3
Kim Pearton <kim.pearton@live.co.uk>
2018-10-23 07:40:13 +0000

    Add delete_player function

--- quiz.py
+++ quiz.py
@@ -29,3 +29,26 @@ def load_file(filename):
     """
     with open(filename, "r") as data:
         return [row for row in data if len(row.strip()) > 0]
+        
+def delete_player(username):
+    """
+    Removes player from players.txt and deletes
+    their incorrect guesses from guesses.txt
+    """
+    with open("data/players.txt", "r+") as player_data:
+        players = player_data.readlines()
+        player_data.seek(0)
+        for line in players:
+            if line != username + "\n":
+                player_data.write(line)
+        player_data.truncate()
+        player_data.close()
+    
+    with open("data/guesses.txt", "r+") as data:
+        f = data.readlines()
+        data.seek(0)
+        for line in f:
+            if username not in line:
+                data.write(line)
+        data.truncate()
+        data.close()
