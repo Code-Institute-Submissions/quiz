52b68dccebdb62990f7bf7485e78c4e2189533f0
Kim Pearton <kim.pearton@live.co.uk>
2018-10-23 07:34:07 +0000

    Add update_guess function

--- quiz.py
+++ quiz.py
@@ -10,3 +10,9 @@ def write_to_file(filename, data):
     """
     with open(filename, "a") as file:
         file.writelines(data)
+        
+def update_guess(username, guess):
+    """
+    Update guesses.txt with player's incorrect guesses
+    """
+    write_to_file("data/guesses.txt", "{0} - {1}\n".format(username, guess))
