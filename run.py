c859f10c14a1abe3d413252897a3046634250854
Kim Pearton <kim.pearton@live.co.uk>
2018-10-23 07:44:09 +0000

    Add render_template for index.html

--- run.py
+++ run.py
@@ -3,3 +3,17 @@ import json
 from flask import Flask, render_template, request, redirect, url_for
 from collections import deque
 import quiz
+
+app = Flask(__name__)
+
+# INDEX PAGE
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+ 
+    # Username POST request
+    if request.method == "POST":
+        quiz.write_to_file("data/players.txt", request.form["username"] + "\n")
+        return redirect(request.form["username"])
+    players = quiz.load_file("data/players.txt")
+    return render_template("index.html", players=players)
