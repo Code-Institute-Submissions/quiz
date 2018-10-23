f0e45d56352b220ef15690b01210426d88937fa9
Kim Pearton <kim.pearton@live.co.uk>
2018-10-23 07:33:33 +0000

    Add write_to_file function

--- quiz.py
+++ quiz.py
@@ -1,2 +1,12 @@
 from collections import deque
 
+"""
+This module will be imported into run.py
+"""
+
+def write_to_file(filename, data):
+    """
+    Write to file
+    """
+    with open(filename, "a") as file:
+        file.writelines(data)
