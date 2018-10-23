299ac02d8875ca48fbb56fea6e098b2b0b23cf40
Kim Pearton <kim.pearton@live.co.uk>
2018-10-23 07:54:08 +0000

    Test the write_name function - PASS

--- test_quiz.py
+++ test_quiz.py
@@ -1,6 +1,22 @@
 import unittest
 import quiz
 
+class TestQuiz(unittest.TestCase):
+    
+    def test_write_name(self):
+        username = "Player123"
+        quiz.write_to_file("data/players.txt", username + "\n")
+        content = open("data/players.txt").read()
+        self.assertIn("Player123", content)
+        #delete test username after use.
+        with open("data/players.txt", "r+") as players:
+            player = players.readlines()
+            players.seek(0)
+            for line in player:
+                if line != "Player123" + "\n":
+                    players.write(line)
+            players.truncate()
+            players.close()
 
 if __name__ == "__main__":
     unittest.main()
