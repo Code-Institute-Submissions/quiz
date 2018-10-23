4b0b83b0da1de9cfeb4971f6987963e1e2ad20b4
Kim Pearton <kim.pearton@live.co.uk>
2018-10-23 07:56:03 +0000

    Test update_score function - PASS

--- test_quiz.py
+++ test_quiz.py
@@ -18,6 +18,24 @@ class TestQuiz(unittest.TestCase):
                     players.write(line)
             players.truncate()
             players.close()
+     
+    # Test if score and username are updated when update_score function called        
+    def test_update_score(self):
+        username = "Player123"
+        score = 10
+        quiz.update_score(username, score)
+        content = open("data/scores.txt").read()
+        self.assertIn("{0} - {1}".format(username, score), content)
+            
+        # Delete Player123 from scores.txt file
+        with open("data/scores.txt", "r+") as player_data:
+            players = player_data.readlines()
+            player_data.seek(0)
+            for line in players:
+                if line != "Player123, 10" + "\n":
+                    player_data.write(line)
+            player_data.truncate()
+            player_data.close()
 
 if __name__ == "__main__":
     unittest.main()
